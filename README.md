927b9d42bccac4b9d2590f2f719f8d18a9f1c113
Renze Nicolai <renze@rnplus.nl>
2022-08-08 20:53:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# ESP32 component: GUI toolkit
+
+Helper functions for quickly drawing a menu, dialog and some other stuff.
+
+Originally part of the MCH2022 launcher firmware but also useful in other apps and firmwares.
+
---
+++ graphics_wrapper.c
@@ -0,0 +1,122 @@
+#include "graphics_wrapper.h"
+
+#include <string.h>
+
+#include "hardware.h"
+#include "pax_keyboard.h"
+
+void render_outline(float position_x, float position_y, float width, float height, pax_col_t border_color, pax_col_t background_color) {
+    pax_buf_t* pax_buffer = get_pax_buffer();
+    pax_simple_rect(pax_buffer, background_color, position_x, position_y, width, height);
+    pax_outline_rect(pax_buffer, border_color, position_x, position_y, width, height);
+}
+
+void render_message(char* message) {
+    pax_buf_t* pax_buffer = get_pax_buffer();
+    const pax_font_t* font    = pax_font_saira_regular;
+    pax_vec1_t        size    = pax_text_size(font, 18, message);
+    float             margin  = 4;
+    float             width   = size.x + (margin * 2);
+    float             posX    = (pax_buffer->width - width) / 2;
+    float             height  = size.y + (margin * 2);
+    float             posY    = (pax_buffer->height - height) / 2;
+    pax_col_t         fgColor = 0xFFfa448c;
+    pax_col_t         bgColor = 0xFFFFFFFF;
+    pax_simple_rect(pax_buffer, bgColor, posX, posY, width, height);
+    pax_outline_rect(pax_buffer, fgColor, posX, posY, width, height);
+    pax_clip(pax_buffer, posX + 1, posY + 1, width - 2, height - 2);
+    pax_center_text(pax_buffer, fgColor, font, 18, pax_buffer->width / 2, ((pax_buffer->height - height) / 2) + margin, message);
+    pax_noclip(pax_buffer);
+}
+
+bool keyboard(float aPosX, float aPosY, float aWidth, float aHeight, const char* aTitle,
+              const char* aHint, char* aOutput, size_t aOutputSize) {
+    pax_buf_t* pax_buffer = get_pax_buffer();
+    const pax_font_t* font     = pax_font_saira_regular;
+    bool              accepted = false;
+    pkb_ctx_t         kb_ctx;
+    pkb_init(pax_buffer, &kb_ctx, 1024);
+    pkb_set_content(&kb_ctx, aOutput);
+    kb_ctx.kb_font   = font;
+    kb_ctx.text_font = font;
+
+    pax_col_t bgColor      = 0xFFFFFFFF;
+    pax_col_t shadowColor  = 0xFFC0C3C8;
+    pax_col_t borderColor  = 0xFF0000AA;
+    pax_col_t titleBgColor = 0xFF080764;
+    pax_col_t titleColor   = 0xFFFFFFFF;
+    pax_col_t selColor     = 0xff007fff;
+
+    kb_ctx.text_col     = borderColor;
+    kb_ctx.sel_text_col = bgColor;
+    kb_ctx.sel_col      = selColor;
+    kb_ctx.bg_col       = bgColor;
+
+    kb_ctx.kb_font_size = 18;
+
+    float titleHeight = 20;
+    float hintHeight  = 14;
+
+    pax_noclip(pax_buffer);
+    pax_simple_rect(pax_buffer, shadowColor, aPosX + 5, aPosY + 5, aWidth, aHeight);
+    pax_simple_rect(pax_buffer, bgColor, aPosX, aPosY, aWidth, aHeight);
+    pax_outline_rect(pax_buffer, borderColor, aPosX, aPosY, aWidth, aHeight);
+    pax_simple_rect(pax_buffer, titleBgColor, aPosX, aPosY, aWidth, titleHeight);
+    pax_simple_line(pax_buffer, titleColor, aPosX + 1, aPosY + titleHeight, aPosX + aWidth - 2, aPosY + titleHeight - 1);
+    pax_clip(pax_buffer, aPosX + 1, aPosY + 1, aWidth - 2, titleHeight - 2);
+    pax_draw_text(pax_buffer, titleColor, font, titleHeight - 2, aPosX + 1, aPosY + 1, aTitle);
+    pax_clip(pax_buffer, aPosX + 1, aPosY + aHeight - hintHeight, aWidth - 2, hintHeight);
+    pax_draw_text(pax_buffer, borderColor, font, hintHeight - 2, aPosX + 1, aPosY + aHeight - hintHeight, aHint);
+    pax_noclip(pax_buffer);
+
+    kb_ctx.x      = aPosX + 1;
+    kb_ctx.y      = aPosY + titleHeight + 1;
+    kb_ctx.width  = aWidth - 2;
+    kb_ctx.height = aHeight - 3 - titleHeight - hintHeight;
+
+    bool running = true;
+    while (running) {
+        input_message_t button_message = {0};
+        if (xQueueReceive(get_input_queue(), &button_message, 16 / portTICK_PERIOD_MS) == pdTRUE) {
+            bool    value = button_message.state;
+            switch (button_message.input) {
+                case INPUT_TOUCH1:
+                    if (value) {
+                        pkb_press(&kb_ctx, PKB_RIGHT);
+                    } else {
+                        pkb_release(&kb_ctx, PKB_RIGHT);
+                    }
+                    break;
+                case INPUT_TOUCH2:
+                    if (value) {
+                        pkb_press(&kb_ctx, PKB_CHARSELECT);
+                    } else {
+                        pkb_release(&kb_ctx, PKB_CHARSELECT);
+                    }
+                    break;
+                case INPUT_TOUCH0:
+                    if (value) {
+                        pkb_press(&kb_ctx, PKB_DELETE_BEFORE);
+                    } else {
+                        pkb_release(&kb_ctx, PKB_DELETE_BEFORE);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        pkb_loop(&kb_ctx);
+        if (kb_ctx.dirty) {
+            pkb_redraw(pax_buffer, &kb_ctx);
+            display_flush();
+        }
+        if (kb_ctx.input_accepted) {
+            memset(aOutput, 0, aOutputSize);
+            strncpy(aOutput, kb_ctx.content, aOutputSize - 1);
+            running  = false;
+            accepted = true;
+        }
+    }
+    pkb_destroy(&kb_ctx);
+    return accepted;
+}
